6e5f55209152512cf9d2cac41576ca664c31b51c
Kaspe <6796782+xunga@users.noreply.github.com>
2018-10-28 19:41:40 +0100

    Added express (#2)

--- server/index.ts
+++ server/index.ts
@@ -1,28 +1,24 @@
-import { createServer } from 'http'
-import { parse } from 'url'
 import * as next from 'next'
+import * as express from 'express'
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+const expressApp = express();
+
+
 app.prepare()
 .then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true)
-    const { pathname, query } = parsedUrl
+  
+  expressApp.get('*', (req, res) => {
+    return handle(req, res);
+  })
 
-    if (pathname === '/a') {
-      app.render(req, res, '/a', query)
-    } else if (pathname === '/b') {
-      app.render(req, res, '/b', query)
-    } else {
-      handle(req, res, parsedUrl)
-    }
+  expressApp.listen(port, () => {
+      console.log(`> Ready on http://localhost:${port}`)
   })
-  .listen(port, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  })
+
+
 })
