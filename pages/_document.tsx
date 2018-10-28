41828e09f41007ab281f65bbd43bb769aa67f855
cryptobaguette <cryptobaguette@tutanota.com>
2018-10-28 20:21:43 +0100

    setup reakit + styled component ssr

---
+++ pages/_document.tsx
@@ -0,0 +1,31 @@
+import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }: any) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage((App: any) => (props: any) =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
+  render() {
+    return (
+      <html lang="en">
+        <Head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          {(this.props as any).styleTags}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
+import { Button } from "reakit";
 
 export default () => (
   <ul>
-    <li><Link href='/a' as='/a'><a>a</a></Link></li>
-    <li><Link href='/b' as='/b'><a>b</a></Link></li>
+    <li>
+      <Link href="/a" as="/a">
+        <a>a</a>
+      </Link>
+    </li>
+    <li>
+      <Link href="/b" as="/b">
+        <a>b</a>
+      </Link>
+    </li>
+    <Button>Button</Button>
   </ul>
-)
+);
